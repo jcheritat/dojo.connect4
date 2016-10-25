bcfb816fad2381adf6caead7ded17df57fa06612
Laurent Yovanovitch <laurent.yovanovitch.pro@outlook.fr>
2016-10-25 14:29:22 +0200

    Iniitalisation, jeu d'un jeton (début)

---
+++ Puissance4/Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Puissance4
+{
+    internal class Game
+    {
+        public int[,] Grille { get; set; }
+        public Game()
+        {
+            Rows = 6;
+            Columns = 7;
+            Grille = new int[6, 7];
+        }
+
+        public int Rows { get; internal set; }
+        public int Columns { get; internal set; }
+
+        internal void DropToken(int user, int column)
+        {
+            Grille[0, 5] = 1;
+            Grille[0, 4] = 2;
+        }
+    }
+}
--- Puissance4/UnitTest1.cs
+++ Puissance4/UnitTest1.cs
@@ -6,9 +6,41 @@ namespace Puissance4
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        private Game puissance4;
+
+        public UnitTest1()
         {
+            puissance4 = new Game();
+        }
+
+        [TestMethod]
+        public void When_A_Game_Is_Created_Grid_Is_Empty()
+        {
+            Assert.AreEqual(6, puissance4.Rows);
+            Assert.AreEqual(7, puissance4.Columns);
+
+            for (var row = 0; row < 6; row++)
+            {
+                for (var col = 0; col < 7; col++)
+                {
+                    Assert.AreEqual(0, puissance4.Grille[row, col]);
+                }
+            }
+        }
+        [TestMethod]
+        public void When_A_Player_Drops_A_Token_Goes_To_Bottom()
+        {
+            puissance4.DropToken(1, 5);
+
+            Assert.AreEqual(1, puissance4.Grille[0, 5]);
+            Assert.AreEqual(0, puissance4.Grille[0, 4]);
+        }
+
+        [TestMethod]
+        public void When_A_Player2_Drops_A_Token_Goes_To_Bottom()
+        {
+            puissance4.DropToken(2, 4);
+            Assert.AreEqual(2, puissance4.Grille[0, 4]);
         }
     }
 }
